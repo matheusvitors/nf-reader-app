52cbf584434ed8e1b859ae5f80ddc92f431bf4c3
Matheus Vitor Sousa Almeida <matheus.almeida@trf1.jus.br>
2025-08-29 14:40:21 -0300

    fiz storage keys

--- src/config/storage-keys.ts
+++ src/config/storage-keys.ts
@@ -2,8 +2,5 @@ const prefix = '@acheitrf1'
 
 export enum KEY {
 	THEME = `${prefix}:theme`,
-	TOKEN = `${prefix}:token`,
-	HISTORICO = `${prefix}:historico`,
-	ACOMPANHAMENTOS = `${prefix}:acompanhamentos`,
 }
 
